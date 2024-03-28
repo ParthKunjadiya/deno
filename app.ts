2f7d876cd17356247a074a8e80f05067183cf214
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-28 15:44:50 +0530

    todo project route setup

--- app.ts
+++ app.ts
@@ -1,16 +1,9 @@
-// const text = "this is a test, and it should be stored in a file!";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-// const encoder = new TextEncoder();
-// const data = encoder.encode(text);
+const app = new Application();
 
-// Deno.writeFile("message.txt", data).then(() => {
-//   console.log("Wrote to file!");
-// });
+app.use((ctx) => {
+  ctx.response.body = "Hello World (Use oak!)";
+});
 
-import { serve } from "https://deno.land/std@0.101.0/http/server.ts";
-
-const server = serve({ port: 3000 });
-
-for await (const req of server) {
-  req.respond({ body: "Hello World (From Deno!)" });
-}
+await app.listen({ port: 8000 });
---
+++ deno/routes/todos.ts
@@ -0,0 +1,45 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+interface Todo {
+  id: string;
+  text: string;
+}
+
+let todos: Todo[] = [];
+
+router.get("/todos", (ctx) => {
+  ctx.response.body = { todos: todos };
+});
+
+router.post("/todos", async (ctx) => {
+  const data = await ctx.request.body.json();
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: data.text,
+  };
+  todos.push(newTodo);
+
+  ctx.response.body = { message: "Created todo!", todo: newTodo };
+});
+
+router.put("/todos/:todoId", async (ctx) => {
+  const tid = ctx.params.todoId;
+  const data = await ctx.request.body.json();
+  const todoIndex = todos.findIndex((todo) => {
+    return todo.id === tid;
+  });
+  todos[todoIndex] = { id: todos[todoIndex].id, text: data.text };
+  ctx.response.body = { message: "Updated Todo!" };
+});
+
+router.delete("/todos/:todoId", (ctx) => {
+  const tid = ctx.params.todoId;
+  todos = todos.filter((todo) => {
+    return todo.id !== tid;
+  });
+  ctx.response.body = { message: "Todo deleted!" };
+});
+
+export default router;
