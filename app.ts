ea7e28527bb45b5394c714b29722596c41a03180
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-03-28 12:07:57 +0530

    deno first code

--- app.ts
+++ app.ts
@@ -1,5 +1,16 @@
-let message: string;
+// const text = "this is a test, and it should be stored in a file!";
 
-message = "Hi, there!";
+// const encoder = new TextEncoder();
+// const data = encoder.encode(text);
 
-console.log(message);
+// Deno.writeFile("message.txt", data).then(() => {
+//   console.log("Wrote to file!");
+// });
+
+import { serve } from "https://deno.land/std@0.101.0/http/server.ts";
+
+const server = serve({ port: 3000 });
+
+for await (const req of server) {
+  req.respond({ body: "Hello World (From Deno!)" });
+}
